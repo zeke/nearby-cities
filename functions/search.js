5a55208b18b8c9809c4ab71bf941e8e993daf076
Zeke Sikelianos <zeke@sikelianos.com>
2020-08-07 21:04:39 -0700

    add netlify function

---
+++ functions/search.js
@@ -0,0 +1,10 @@
+// This is a Netlify serverless function
+// 
+// https://docs.netlify.com/functions/build-with-javascript
+
+exports.handler = function(event, context, callback) {
+  callback(null, {
+  statusCode: 200,
+  body: "Hello, World"
+  })
+}
