097acec69bd9cb4dcd8cf7535e67812be023fbc1
Zeke Sikelianos <zeke@sikelianos.com>
2020-08-07 22:46:02 -0700

    allow CORS

--- functions/search/search.js
+++ functions/search/search.js
@@ -4,20 +4,25 @@
 
 const nearbyCities = require("nearby-cities")
 const maxResults = 100
+const headers = {
+  'Access-Control-Allow-Origin': '*'
+}
 
 exports.handler = async function (event, context) {
   if (!event.queryStringParameters.latitude || !event.queryStringParameters.longitude) {
     return {
       statusCode: 400,
+      headers,
       body: JSON.stringify({error: 'latitude and longitude query params are required'})
     }
   }
 
   const { latitude, longitude } = event.queryStringParameters
-  const cities = nearbyCities({latitude, longitude}).slice(0, 100)
+  const cities = nearbyCities({latitude, longitude}).slice(0, maxResults)
 
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify(cities, null, 2)
   }
 }
